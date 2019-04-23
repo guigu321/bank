4131a725e710012a73854cb618da2d78246d1eef
Administrator <Administrator@PC-20171010XEED>
2019-04-23 10:53:35 +0800

    xxx

--- Bank/src/main/java/com/guigui/bank/service/TransactionRecordService.java
+++ Bank/src/main/java/com/guigui/bank/service/TransactionRecordService.java
@@ -2,10 +2,13 @@ package com.guigui.bank.service;
 
 import java.util.List;
 
+import com.guigu.pojo.TransactionDate;
 import com.guigu.pojo.TransactionRecord;
 import com.guigu.pojo.TransactionRecordExample;
 
 public interface TransactionRecordService {
 
 	List<TransactionRecord> selectByExample(TransactionRecordExample example);
+	
+	List<TransactionRecord> selectTransaction(TransactionDate date);
 }
